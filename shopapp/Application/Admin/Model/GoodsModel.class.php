185868daf4d707089e4e0944be2f9fe5d3110022
wenbie <wenbie888@gmail.com>
2017-09-03 22:18:57 +0800

    no message

---
+++ shopapp/Application/Admin/Model/GoodsModel.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lin
+ * Date: 17/9/3
+ * Time: 上午8:59
+ */
+
+namespace Admin\Model;
+use Think\Model;
+
+class GoodsModel extends Model {
+
+}
---
+++ shopapp/Application/Admin/Model/QqModel.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lin
+ * Date: 17/9/3
+ * Time: 上午9:02
+ */
+
+namespace Admin\Model;
+use Think\Model;
+
+class QqModel extends Model {
+    protected $trueTableName    =   'tencent_qq';
+}
